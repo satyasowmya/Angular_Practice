064392d6854adf8c649f92232216d0b3f6304770
Sowmya <satyasowmya0@gmail.com>
2022-12-27 22:38:29 -0800

    reactive from

---
+++ reactiveform/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
+
+
+import { AppComponent } from './app.component';
+import { EmployeeFormComponent } from './employee-form/employee-form.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    EmployeeFormComponent
+  ],
+  imports: [
+    BrowserModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
